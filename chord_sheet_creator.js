56b602fbc541b7159f3d5e4505527b60239b4f4f
Olie Ryan-George <23618262+occrg@users.noreply.github.com>
2025-08-10 15:23:47 +0100

    song details can be added to chord chart

--- chord_sheet_creator.js
+++ chord_sheet_creator.js
@@ -8,6 +8,7 @@ const CHORD_SHEET_TEMPLATE_HTML = `
     <div class="page">
         <h1 id="song-title"></h1>
         <h2 id="song-artist"></h2>
+        <p id="song-details"></p>
     </div>
 </body>
 `
@@ -15,6 +16,25 @@ const CHORD_SHEET_TEMPLATE_HTML = `
 function insertSongDetails(chordSheetHTML) {
     chordSheetHTML.querySelector("#song-title").innerHTML = document.getElementById("song-title-input").value;
     chordSheetHTML.querySelector("#song-artist").innerHTML = document.getElementById("song-artist-input").value;
+
+    songDetailsInnerHTML = "";
+    if (document.getElementById("song-key-input").value) {
+        songDetailsInnerHTML += `Key: ${document.getElementById("song-key-input").value}`;
+    }
+    if (document.getElementById("song-bpm-input").value) {
+        if (songDetailsInnerHTML != "") songDetailsInnerHTML += "; ";
+        songDetailsInnerHTML += `Tempo: ${document.getElementById("song-bpm-input").value}bpm`;
+    }
+    if (document.getElementById("song-time-signature-input").value) {
+        if (songDetailsInnerHTML != "") songDetailsInnerHTML += "; ";
+        songDetailsInnerHTML += `Time: ${document.getElementById("song-time-signature-input").value}`;
+    }
+    if (songDetailsInnerHTML == "") {
+        chordSheetHTML.querySelector("#song-details").remove()
+    } else {
+        chordSheetHTML.querySelector("#song-details").innerHTML = songDetailsInnerHTML
+    }
+
     return chordSheetHTML;
 }
 
