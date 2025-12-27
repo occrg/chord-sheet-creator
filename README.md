fc1f1adc434e6150fcd101ad166cd274f1da8f57
Olie Ryan-George <23618262+occrg@users.noreply.github.com>
2025-12-27 20:10:46 +0000

    html now downloads with full styling

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 A tool that assists in the creation of chord simple and compact chord sheets.
 
 ## Current functionality
-You can enter the title, artists, key, bpm, time signature and lyrics of a song and have them outputted into the console log as HTML ina  neat format so chords can be added separately.
+You can enter the title, artists, key, bpm, time signature and lyrics of a song and have them outputted as a HTML file so chords can be added separately.
 
 ## Non-functional notes
 * Screen sizes from 320px to 1728px supported.
@@ -11,10 +11,11 @@ You can enter the title, artists, key, bpm, time signature and lyrics of a song 
 ## Future functionality
 1. Add chords directly in the tool.
 1. Include button helpers for symbols and potentially common notation (flat, sharp).
-1. Allow PDF download of finished chord sheet (and HTML download).
-1. Ability to cope with more than one page.
 1. Allow export and import of data so user can pick up where they left off.
 1. Host this online.
+1. Allow PDF download of finished chord sheet.
+1. Ability to cope with more than one page.
+1. About page explaining what the project is, why I did it and how.
 1. Full AAA accessibility.
 
 ## Pre-requisites
@@ -24,19 +25,19 @@ None.
 1. Run Vue project.
 1. Type in the title of the song and paste in the lyrics.
 1. Optionally, type in the artist, key, bpm, time signature of the song.
-1. Go to the console log and copy the HTML.
-1. Paste the HTML into a file ending in `Chord Sheet.html` so it's ignored by git and place the file in the `chord_sheet` directory.
-1. Add the chords to the HTML.
+1. Click to download the HTML file.
+1. Open the HTML in text editor and add the chords to the HTML.
 1. Open the HTML file in a browser and save it as a PDF.
 
 ## To Do
 This is a list of things to do for the project, not including implementing the "future functionality" above.
+1. Ensure names of file downloads are based on the song details.
+1. Refactor folder structure so Vue project isn't unnecessarily one folder down.
 1. Fix issue with resubmitting lyrics resulting in adding lines rather than replacing.
 1. Ensure consistent code formatting (including indentation, order of setup/lang).
 1. Ensure HTML elements are identifiable from dev tools.
 1. Ensure appropriate HTML elements are used everywhere.
 1. Refactor convoluted way to call complete functions.
-1. Simplify code so similar logic isn't repeated in different places. Especially with regards to chord sheet preview as inconsistencies between preview and download is bad UX.
 1. Move URLs linked in application to be environment variables.
 1. Add animations for toggling of accorion (accordion open and chevron rotating).
 1. Abstract styling of ChevronIcon so it can be used more flexibly later.
@@ -45,3 +46,4 @@ This is a list of things to do for the project, not including implementing the "
 1. Potentially: Use the Composition API over the Options API for Pinia.
 1. Potentially: Separate out accordion logic into own library. 
 1. Ensure long lines work well.
+1. Use consts wherever possible.
--- chord_sheet_creator/src/components/chord_sheet_controls/ChordSheetControls.vue
+++ chord_sheet_creator/src/components/chord_sheet_controls/ChordSheetControls.vue
@@ -24,36 +24,121 @@ import Button from "../reusable/Button.vue";
 </template>
 
 <script lang="ts">
+const CSS_CHORD_SHEET_FILEPATH = "/public/chord_sheet.css";
+const CSS_CHORD_SHEET_DOWNLOAD_FILEPATH = "/public/chord_sheet_download.css";
+
 export default {
   name: "chord-sheet-controls",
   computed: {
     ...mapState(useDOMStore, ["chordSheetPreviewRef"])
   },
   methods: {
-    downloadHTML: function () {
+    downloadHTML: async function () {
       if (this.chordSheetPreviewRef == null)
         throw new Error("Chord sheet preview page element not found in storage");
-      let chordSheetPreviewForExport = this.chordSheetPreviewRef.cloneNode(true) as HTMLElement;
+      let chordSheetPreviewForExport: HTMLElement = this.prepareElementForHTMLDownload(this.chordSheetPreviewRef)
+      let styledchordSheetPreviewForExport = await this.amendHTMLStyling(chordSheetPreviewForExport);
+      this.downloadElementAsHTML("chord_sheet.html", styledchordSheetPreviewForExport);
+    },
+    prepareElementForHTMLDownload: function (elementToDownload: HTMLElement): HTMLElement {
+      let htmlElement = document.createElement("html");
 
-      let chordSheetPreviewPageContentElement = chordSheetPreviewForExport.children[0];
-      if (chordSheetPreviewPageContentElement == null ||
-        chordSheetPreviewPageContentElement.className !== "page-content")
-      throw new Error("Chord sheet preview page element not as expected");
+      let headElement = document.createElement("head");
+      let styleElement = document.createElement("style");
+      headElement.appendChild(styleElement);
       
+      let bodyElement = document.createElement("body");
+      let elementToDownloadClone = elementToDownload.cloneNode(true) as HTMLElement;
+      bodyElement.appendChild(elementToDownloadClone);
+
+      htmlElement.appendChild(headElement);
+      htmlElement.appendChild(bodyElement);
+
+      return htmlElement;
+    },
+    amendHTMLStyling: async function (chordSheetPreviewForExport: HTMLElement): Promise<HTMLElement> {
+      // Removes style of page-content div as this change the size of content based on screen size in web app but HTML
+      // always shows as A4 size.
+      chordSheetPreviewForExport = this.removeStyleAttributeFromPageContentDiv(chordSheetPreviewForExport);
+
+      // Adds CSS styles directly to HTML so file can be downloaded as one simple HTML file.
+      try {
+        chordSheetPreviewForExport = 
+          await this.addCSSFileAsStyle(CSS_CHORD_SHEET_FILEPATH, chordSheetPreviewForExport);
+        chordSheetPreviewForExport = 
+          await this.addCSSFileAsStyle(CSS_CHORD_SHEET_DOWNLOAD_FILEPATH, chordSheetPreviewForExport);
+      } catch (error) {
+        const errorMessage = (error as Error).message;
+        throw new Error(errorMessage);
+      }
+
+      return chordSheetPreviewForExport;
+    },
+    removeStyleAttributeFromPageContentDiv: function (chordSheetPreviewForExport: HTMLElement): HTMLElement {
+      const chordSheetPreviewPageContentElements = chordSheetPreviewForExport.getElementsByClassName("page-content");
+      if (chordSheetPreviewPageContentElements.length != 1)
+        throw new Error("Chord sheet preview page element not as expected");      
+      const chordSheetPreviewPageContentElement = chordSheetPreviewPageContentElements[0] as HTMLElement;
       chordSheetPreviewPageContentElement.removeAttribute("style");
-
-      let filename = "chord_sheet.html";
-      let element = document.createElement("a");
-      element.setAttribute("href", "data:application/html;charset=utf-8," + encodeURIComponent(chordSheetPreviewForExport.outerHTML));
-      element.setAttribute("download", filename);
-
-      element.style.display = "none";
-      document.body.appendChild(element);
+      return chordSheetPreviewForExport;
+    },
+    addCSSFileAsStyle: async function (filepath: string, elementToStyle: HTMLElement): Promise<HTMLElement> {
+      const startOfCSSInCSSFile = `const __vite__css = "`;
+      const endOfCSSInCSSFileIndicator = `}"`;
+      const firstCharacterAfterEndOfCSSInCSSFile = `"`;
       
-      element.click();
-      document.body.removeChild(element); 
+      const response = await fetch(filepath)
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
       
-      console.log(chordSheetPreviewForExport.outerHTML);
+      const cssFileText = await response.text();
+
+      const afterStartOfCSSInCSSFileSplit: string[] = cssFileText.split(startOfCSSInCSSFile);
+      if (afterStartOfCSSInCSSFileSplit.length != 2)
+        throw new Error(`CSS file not in expected shape when splitting by 
+        startOfCSSInCSSFile: ${startOfCSSInCSSFile}. cssFileText: ${cssFileText}`);
+      const afterStartOfCSSInCSSFile: string = afterStartOfCSSInCSSFileSplit[1] as string;
+
+      const beforeEndOfCSSInCSSFileSplit: string[] = afterStartOfCSSInCSSFile.split(endOfCSSInCSSFileIndicator);
+      if (beforeEndOfCSSInCSSFileSplit.length != 2)
+        throw new Error(`CSS file not in expected shape when splitting by 
+        endOfCSSInCSSFileIndicator: ${endOfCSSInCSSFileIndicator}. 
+        afterStartOfCSSInCSSFile: ${afterStartOfCSSInCSSFile}`);
+      const beforeEndOfCSSInCSSFile: string = beforeEndOfCSSInCSSFileSplit[0] as string;
+      
+      // Adding back the part of the delimiter that is part of the CSS.
+      const endOfCSSInCSSFileIndicatorSplit: string[] = 
+      endOfCSSInCSSFileIndicator.split(firstCharacterAfterEndOfCSSInCSSFile);
+      if (endOfCSSInCSSFileIndicatorSplit.length < 2)
+        throw new Error(`Constants not as expected. 
+        Splitting endOfCSSInCSSFileIndicator: ${endOfCSSInCSSFileIndicator} by 
+        firstCharacterAfterEndOfCSSInCSSFile: ${firstCharacterAfterEndOfCSSInCSSFile}
+        doesn't result in a 2 or more element array.`);
+      const charactersToAddBackToCSSString: string = endOfCSSInCSSFileIndicatorSplit[0] as string;
+
+      const cssContentUnformatted = beforeEndOfCSSInCSSFile + charactersToAddBackToCSSString;
+            
+      const styleElements = elementToStyle.getElementsByTagName("style");
+      if (styleElements.length != 1)
+        throw new Error("Constructed HTML element does not include exactly one style element");
+      let styleElement = styleElements[0] as HTMLElement;
+      const cssContent = cssContentUnformatted.replace(/\\n|\\/g, "");
+      styleElement.innerHTML += cssContent;
+
+      return elementToStyle;
+    },
+    downloadElementAsHTML: function (filename: string, elementToDownload: HTMLElement) {
+      let downloadHelperElement = document.createElement("a");
+      downloadHelperElement.setAttribute("href", 
+        "data:application/html;charset=utf-8," + encodeURIComponent(elementToDownload.outerHTML));
+      downloadHelperElement.setAttribute("download", filename);
+
+      downloadHelperElement.style.display = "none";
+      document.body.appendChild(downloadHelperElement);
+      
+      downloadHelperElement.click();
+      document.body.removeChild(downloadHelperElement); 
     }
   }
 };
